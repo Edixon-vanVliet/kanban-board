e38b4de7e305271bb1bf5687fe312f3f916b0f68
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-18 16:52:53 -0400

    Create tags to be used

---
+++ src/utils/tags.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faClipboardList } from "@fortawesome/free-solid-svg-icons";
+import { faComments } from "@fortawesome/free-regular-svg-icons";
+
+export const tags = Object.freeze({
+  seo: "SEO",
+  longForm: "Long Form",
+  blogPost: "Blog Post",
+});
+
+export const tagIcons = Object.freeze({
+  [tags.seo]: <FontAwesomeIcon icon={faMagnifyingGlass} key={tags.seo} title={tags.seo} />,
+  [tags.longForm]: <FontAwesomeIcon icon={faClipboardList} key={tags.longForm} title={tags.longForm} />,
+  [tags.blogPost]: <FontAwesomeIcon icon={faComments} key={tags.blogPost} title={tags.blogPost} />,
+});
