d52c20e363132981bc8d7cb09cd2a1899617a274
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-18 17:42:01 -0400

    Add react to return icons

--- src/utils/tags.js
+++ src/utils/tags.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faClipboardList } from "@fortawesome/free-solid-svg-icons";
 import { faComments } from "@fortawesome/free-regular-svg-icons";
